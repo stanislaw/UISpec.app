c508f3db6d7477510b512668735996992d245caa
Stanislaw Pankevich <s.pankevich@gmail.com>
2014-11-08 17:34:25 +0300

    Add Xcode project

---
+++ PixelPerfect/PixelPerfect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PixelPerfect
+//
+//  Created by Stanislaw Pankevich on 08/11/14.
+//  Copyright (c) 2014 Stanislaw Pankevich. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PixelPerfect/PixelPerfect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PixelPerfect
+//
+//  Created by Stanislaw Pankevich on 08/11/14.
+//  Copyright (c) 2014 Stanislaw Pankevich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
