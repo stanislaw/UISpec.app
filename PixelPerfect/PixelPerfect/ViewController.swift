ccb40d3a87a58be110d7f0806bba9444e3db1bdb
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-05-19 00:00:28 +0300

    Connect imageView outlet

--- PixelPerfect/PixelPerfect/ViewController.swift
+++ PixelPerfect/PixelPerfect/ViewController.swift
@@ -10,10 +10,12 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        println(imageView)
     }
 
     override var representedObject: AnyObject? {
