02801be43f48c6df8744d8e33dbc35f3870a5ba7
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-08-21 21:14:35 +0200

    Introduce System and Use Cases folders

---
+++ PixelPerfect/PixelPerfect/Classes/System/Mac.swift
@@ -0,0 +1,13 @@
+//
+//  Mac.swift
+//  PixelPerfect
+//
+//  Created by Stanislaw Pankevich on 21/08/15.
+//  Copyright (c) 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+import Cocoa
+
+class Mac: NSObject {
+
+}
